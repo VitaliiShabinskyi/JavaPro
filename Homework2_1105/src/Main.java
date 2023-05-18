4ac32b3f44e06fb10919f27af8b009d28c773c47
Vitalii.Shabinskyi <vetalsoft@gmail.com>
2023-05-19 01:01:19 +0200

    Homework 11.05 (Singlton)

---
+++ Homework2_1105/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        TelRan telRan = TelRan.getInstance();
+        telRan.init();
+        telRan.performLection();
+
+    }
+}
